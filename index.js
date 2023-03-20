1623c25868b3c68a0363e3267eacc0c2e658bb42
WendyTeo <wendy.teo@iag.com.au>
2023-03-20 17:43:19 +1100

    added basic code to fetch openAI API

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
+import { config } from 'dotenv';
+config();
 
-console.log("hello world!");
+import { Configuration, OpenAIApi } from 'openai';
+import readline from 'readline';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openAI = new OpenAIApi(configuration);
+
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+userInterface.prompt();
+userInterface.on('line', async (input) => {
+  const res = await openAI.createCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: input }],
+    temperature: 0,
+    max_tokens: 7,
+  });
+  console.log(res.data);
+  // console.log(res.data.choices[0].message.content);
+  userInterface.prompt();
+});
+
+
+
+
+
+
