56b228aa0fdac77ad54ad3bda4bf8a5461354c31
WendyTeo <wendy.teo@iag.com.au>
2023-03-22 09:18:54 +1100

    tidied the console.log

--- index.js
+++ index.js
@@ -19,10 +19,12 @@ userInterface.prompt();
 userInterface.on('line', async (input) => {
   const res = await openAI.createChatCompletion({
     model: 'gpt-3.5-turbo',
-    messages: [{ role: 'system', content: 'You are a helpful asistant who teaches junior developers and checks on their code for optimization.'}, { role: 'user', content: input }],
+    messages: [{ role: 'system', content: 'You are a helpful asistant who teaches junior developers and checks on their code for optimization.'}, 
+    { role: 'user', content: input }],
   });
-  console.log('AI Asistant');
-  console.log(`AI Asistant: ${res.data.choices[0].message.content}`);
+  console.log('AI Asistant:');
+  console.log(res.data.choices[0].message.content);
+  consolw.log(' ');
 });
 
 
