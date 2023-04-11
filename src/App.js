6c5821ed78f1a0448060e860bc0e0ec7ecde9a01
Wendy Teo <wendyteo.wy@gmail.com>
2023-04-12 07:59:12 +1000

    Included array messages

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
 
   const [userInput, setUserInput] = useState('');
   const [response, setResponse] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  // Previous messages in conversation with AI is saved in this array
+  const [messages, setMessages] = useState([]);
 
   const userInputHandler = (event) => {
     setUserInput(event.target.value);
@@ -17,57 +20,48 @@ function App() {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    // setUserInput('');
+    setIsSubmitted(false);
+    setUserInput('');
   };
 
+  const clickHandler = () => {
+    setIsSubmitted(true);
+  }
+
   useEffect(() => {
-    let timeoutId;
-  
-    const handleKeyPress = (event) => {
-      if (event.key === 'Enter') {
-        clearTimeout(timeoutId);
-        fetchData();
-      }
-    };
-  
-    const handleClick = () => {
-      clearTimeout(timeoutId);
-      fetchData();
-    };
-  
-    const fetchData = async () => {
+    const fetchData = async() => {
       const res = await openAI.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: [
           { role: 'system', content: 'You are a helpful assistant who teaches junior developers and checks on their code for optimization.'}, 
-          { role: 'user', content: userInput }
+          { role: 'user', content: `${messages.join(' ')} ${userInput}` }
         ],
-      });
+      })
+
       const message = res.data.choices[0].message.content;
       setResponse(message);
     };
   
-    document.addEventListener('keydown', handleKeyPress);
-    const button = document.querySelector('button');
-    button.addEventListener('click', handleClick);
-  
-    return () => {
-      document.removeEventListener('keydown', handleKeyPress);
-      button.removeEventListener('click', handleClick);
-      clearTimeout(timeoutId);
-    };
-  }, [userInput]);
+    if (isSubmitted && userInput !== '') {
+      fetchData();
+      setMessages([...messages, response, userInput]);
+      setResponse('Generating messages...');
+      console.log(messages);
+    }
+  }, [userInput, isSubmitted, messages, response]);
   
   
 
   return (
     <div id="chat">
+      <h1>Welcome, Junior Developers</h1>
       <ul>
+        { messages.map((message) => <li>{ message }</li>) }
         <li>{ response  }</li>
       </ul>
       <form onSubmit={ submitHandler} id="user-input">
         <input onChange={ userInputHandler } value={ userInput } type="text" />
-        <button type="submit">Enter</button>
+        <button onClick={ clickHandler} type="submit">Enter</button>
       </form>
     </div>
   );
